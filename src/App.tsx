a0aa4f667f9db27f746e0f129dd6cb246f10faaf
PavanVaranasi2 <pavankumar.varanasi@zemosolabs.com>
2024-08-07 18:49:47 +0530

    feat: search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import TaskSearch from "./components/organisms/TaskSearch";
 
 const App: React.FC = () => {
-  return <></>;
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Task Search
+      </Typography>
+      <TaskSearch />
+    </Container>
+  );
 };
 
 export default App;
